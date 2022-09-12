e59fdb26dab016a3e9efe62182a4be198fa3f056
unknown <sonia8930@gmail.com>
2022-09-13 00:19:29 +0530

    Removing unused imports

--- src/main/java/com/dockerdemo/DockerdemoApplication.java
+++ src/main/java/com/dockerdemo/DockerdemoApplication.java
@@ -2,18 +2,9 @@ package com.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 @SpringBootApplication
-@EnableConfigurationProperties
-/*@EnableJpaRepositories(basePackages = "com.dockerdemo.repository.CustomerRepository")
-@EnableTransactionManagement*/
-/*@EntityScan(basePackages = {"com.dockerdemo.Entity.Customer","com.dockerdemo.Entity.CustomerAddress"})*/
 public class DockerdemoApplication {
 
 	public static void main(String[] args) {
